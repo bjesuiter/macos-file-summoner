68fdeacb8e12570139a317bb566880b31b4cbabe
Benjamin Jesuiter <benjamin.jesuiter@flavia-it.de>
2020-05-20 17:21:06 +0200

    add code to main.go to touch new file in current working dir

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"log"
+	"os"
 )
 import "github.com/andybrewer/mack"
+import "os/exec"
+
+func touchFile(path string, filename string) error {
+	cmd := exec.Command("touch", filename)
+	cmd.Dir = path
+	return cmd.Run()
+}
 
 func main() {
 
-	out, err := mack.Tell("Finder",
+	path, err := mack.Tell("Finder",
 		"if (count of Finder windows) is 0 then",
 		"set dir to (desktop as alias)",
 		"else",
@@ -15,8 +23,26 @@ func main() {
 		"end if",
 		"return POSIX path of dir")
 	if err != nil {
-		log.Fatalf("Error:  %s", err)
+		panic(err)
 	}
 
-	log.Printf("Topmost finder path:  %s\n", out)
+	log.Printf("Topmost finder path:  %s\n", path)
+
+	response, err := mack.Dialog("New Filename", "Insert Filename", "newFile.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	if response.Clicked == "Cancel" {
+		// handle the Cancel event
+		os.Exit(2)
+	} else {
+		newFilename := response.Text
+		err := touchFile(path, newFilename)
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("File Created: %s",newFilename)
+	}
+
 }
