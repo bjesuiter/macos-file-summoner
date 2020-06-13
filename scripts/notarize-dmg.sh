3a76ff341f0b8d0755e084e006256e49db622374
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-06-13 21:34:47 +0200

    add script to notarize dmg

--- scripts/notarize-dmg.sh
+++ scripts/notarize-dmg.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# This action needs the APPLE_ACCOUNT_APP_PASSWORD variable set with an App Password for your apple account
 echo '-- Notarize dmg --'
 # Notarize dmg locally with gon ./scripts/gon-dmg-local.hcl 
-gon ./scripts/gon-dmg.hcl   
+gon ./scripts/gon-dmg.json   
