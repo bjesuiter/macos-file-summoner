f04ee15f2a2abe8f2652c35ae5d370b4ff3beb68
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-06-13 21:47:41 +0200

    improve notarization script to auto-update the path to the dmg file which should be notarized

--- scripts/notarize-dmg.sh
+++ scripts/notarize-dmg.sh
@@ -2,5 +2,10 @@
 
 # This action needs the APPLE_ACCOUNT_APP_PASSWORD variable set with an App Password for your apple account
 echo '-- Notarize dmg --'
+
+# This command needs the npm package json to be installed
+echo 'Update path for dmg to notarize in gon-dmg.json config'
+json -I -f scripts/gon-dmg.json -e "this.notarize[0].path='File Summoner $(cat ./version).dmg'"
+
 # Notarize dmg locally with gon ./scripts/gon-dmg-local.hcl 
 gon ./scripts/gon-dmg.json   
