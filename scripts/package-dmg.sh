e65e442d8dd5c611fd246984868755e8285f5926
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-06-13 21:33:39 +0200

    remove shell param usage in package-dmg

--- scripts/package-dmg.sh
+++ scripts/package-dmg.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# This input param should be filled with the secrets.APPLE_DEVELOPER_ID_NAME on gihub actions
-APPLE_DEVELOPER_ID_NAME=$1
+# This env var should be filled with the secrets.APPLE_DEVELOPER_ID_NAME on gihub actions
 APPLE_DEVELOPER_ID_NAME=${APPLE_DEVELOPER_ID_NAME:-Developer ID Application: Benjamin Jesuiter (BB38WRH6VJ)}
 
 echo '-- Package dmg --'
