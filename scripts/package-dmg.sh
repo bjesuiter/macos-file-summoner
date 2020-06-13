929f82f0d0ba820e40fafa0a9113ce2c5b883c9e
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-06-13 21:19:14 +0200

    fix package-dmg var handling

--- scripts/package-dmg.sh
+++ scripts/package-dmg.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # This input param should be filled with the secrets.APPLE_DEVELOPER_ID_NAME on gihub actions
-APPLE_DEVELOPER_ID_NAME=${1:-Developer ID Application: Benjamin Jesuiter (BB38WRH6VJ)}
+APPLE_DEVELOPER_ID_NAME=$1
+APPLE_DEVELOPER_ID_NAME=${APPLE_DEVELOPER_ID_NAME:-Developer ID Application: Benjamin Jesuiter (BB38WRH6VJ)}
 
 echo '-- Package dmg --'
 DIST_DIR=${DIST_DIR:-dist}
