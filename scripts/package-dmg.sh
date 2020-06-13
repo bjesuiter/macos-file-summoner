3c4825fdd347de7dd4279ca93d88695cf3b5e9ff
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-06-13 20:41:45 +0200

    add code signing to github actions workflow + rename env vars to more suitable syntax

--- scripts/package-dmg.sh
+++ scripts/package-dmg.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This input param should be filled with the secrets.APPLE_DEVELOPER_ID_NAME on gihub actions
+APPLE_DEVELOPER_ID_NAME=${1:-Developer ID Application: Benjamin Jesuiter (BB38WRH6VJ)}
+
 echo '-- Package dmg --'
 DIST_DIR=${DIST_DIR:-dist}
-create-dmg --overwrite ${DIST_DIR}/*.app --identity="Developer ID Application: Benjamin Jesuiter (BB38WRH6VJ)"
+create-dmg --overwrite ${DIST_DIR}/*.app --identity="${APPLE_DEVELOPER_ID_NAME}"
