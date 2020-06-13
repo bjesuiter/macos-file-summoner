c9439731634ee58e2b7247bc70f792ddf70a2b04
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-06-13 15:17:03 +0200

    update package-dmg

--- scripts/package-dmg.sh
+++ scripts/package-dmg.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+echo '-- Package dmg --'
 DIST_DIR=${DIST_DIR:-dist}
 create-dmg --overwrite ${DIST_DIR}/*.app --identity="Developer ID Application: Benjamin Jesuiter (BB38WRH6VJ)"
