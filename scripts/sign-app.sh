5af7409ac060aa48e19052441b47088a4c48b940
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-06-13 15:14:21 +0200

    uodate script to sign apps

---
+++ scripts/sign-app.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+DIST_DIR=${DIST_DIR:-dist}
+MACOS_APP_ARTIFACT=${MACOS_APP_ARTIFACT:-'File Summoner.app'}
+
+echo '> sign mac app ...'
+# Codesign is a xcode utility to signing mac apps via comand line
+# Note: the '--options runtime' flag enables the macOS hardened runtime, 
+#       which is needed for notarization
+codesign --deep --force --verbose --options runtime --sign "BB38WRH6VJ" ${DIST_DIR}/*.app
+
+echo '> validate signature itself ...'
+
+# Check, whether the signature is valid 
+codesign --verify -vvvv ${DIST_DIR}/*.app
+
+# Check, whether the certificate used for the signature is valid
+# Not working anymore, since apple requires notarization
+# echo '> validate certificate ...'
+# spctl -a -vvvv ${DIST_DIR}/*.app
