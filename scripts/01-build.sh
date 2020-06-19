a845498d17dca6e1b48a9bd0fd2e546293c3f175
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-06-19 23:03:04 +0200

    improve build script macos app template copying

--- scripts/01-build.sh
+++ scripts/01-build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+function log() {
+    echo 
+    echo "${1}"
+}
+
 # Input Vars
 DIST_DIR=${DIST_DIR:-dist}
 MACOS_APP_ARTIFACT=${MACOS_APP_ARTIFACT:-'File Summoner.app'}
@@ -8,30 +13,30 @@ MACOS_APP_ARTIFACT=${MACOS_APP_ARTIFACT:-'File Summoner.app'}
 EXECUTABLE=macos-file-summoner
 BUILD_DIR=build
 
-echo 'Check GOOS and GOARCH before setting the variables...'
+log 'Check GOOS and GOARCH before setting the variables...'
 go env GOOS GOARCH
 
 export GOOS=darwin
 export GOARCH=amd64
 
-echo 'Check GOOS and GOARCH after setting the variables...'
+log 'Check GOOS and GOARCH after setting the variables...'
 go env GOOS GOARCH
 
-echo 'build go executable...'
+log 'build go executable...'
 go build -o ${BUILD_DIR}/${EXECUTABLE} .
 
-echo 'clean dist folder...'
+log 'clean dist folder...'
 rm -rf ${DIST_DIR}
 mkdir ${DIST_DIR}
 
-echo 'copy mac-app-template/File Summoner.app to dist...'
-cp -R "mac-app-template/File Summoner.app" ${DIST_DIR}/
+log "create target folder: ${DIST_DIR}/${MACOS_APP_ARTIFACT}/"
+mkdir -p "${DIST_DIR}/${MACOS_APP_ARTIFACT}/"
 
-# Rename the distribution App to the name defined in MACOS_APP_ARTIFACT
-mv "${DIST_DIR}/File Summoner.app" "${DIST_DIR}/${MACOS_APP_ARTIFACT}"
+log "copy mac-app-template/* to ${DIST_DIR}/${MACOS_APP_ARTIFACT}/"
+cp -a "mac-app-template/." "${DIST_DIR}/${MACOS_APP_ARTIFACT}/"
 
-echo "copy go binary into ${DIST_DIR}/${MACOS_APP_ARTIFACT}/Contents/MacOS/"
+log "copy go binary into ${DIST_DIR}/${MACOS_APP_ARTIFACT}/Contents/MacOS/${EXECUTABLE}"
 cp ${BUILD_DIR}/${EXECUTABLE} "${DIST_DIR}/${MACOS_APP_ARTIFACT}/Contents/MacOS/"
 
-echo "cleanup placeholder for binary: ${DIST_DIR}/${MACOS_APP_ARTIFACT}/Contents/MacOS/.insert-binary-here"
+log "cleanup placeholder for binary: ${DIST_DIR}/${MACOS_APP_ARTIFACT}/Contents/MacOS/.insert-binary-here"
 rm -f "${DIST_DIR}/${MACOS_APP_ARTIFACT}/Contents/MacOS/.insert-binary-here"
