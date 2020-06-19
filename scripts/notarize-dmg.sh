d9507230ed5ca9d4c0b9b741ce24eafffeee2ea5
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-06-20 00:44:48 +0200

    reimplement package & deploy jobs + test the complete workflow via tag v1.2.0-prerelease.1

--- scripts/notarize-dmg.sh
+++ scripts/notarize-dmg.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-# This action needs the APPLE_ACCOUNT_APP_PASSWORD variable set with an App Password for your apple account
-echo '-- Notarize dmg --'
+function log() {
+    echo 
+    echo "${1}"
+}
 
-# This command needs the npm package json to be installed
-echo 'Update path for dmg to notarize in gon-dmg.json config'
+###   Dependencies for this script       
+### - an Apple Account App Password for the developer account used to sign the dmg!
+### - https://www.npmjs.com/package/json (can be globally installed with npm)
+
+# logs the script name
+log "-- $(basename ${0}) --"
+
+log 'Update path for dmg to notarize in gon-dmg.json config'
 json -I -f scripts/gon-dmg.json -e "this.notarize[0].path='File Summoner $(cat ./version).dmg'"
 
 # Notarize dmg locally with gon ./scripts/gon-dmg-local.hcl 
--- scripts/package-dmg.sh
+++ scripts/package-dmg.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+###   Dependencies for this script       
+### - an Apple Developer ID Certificate available inside macOS Keychain
+### - https://www.npmjs.com/package/create-dmg (can be globally installed with npm)
+
+function log() {
+    echo 
+    echo "${1}"
+}
+
+# Input Vars
 # This env var should be filled with the secrets.APPLE_DEVELOPER_ID_NAME on gihub actions
 APPLE_DEVELOPER_ID_NAME=${APPLE_DEVELOPER_ID_NAME:-Developer ID Application: Benjamin Jesuiter (BB38WRH6VJ)}
 
-echo '-- Package dmg --'
+# logs the script name
+log "-- $(basename ${0}) --"
+
 DIST_DIR=${DIST_DIR:-dist}
+# The resulting DMG file will have the form of 'File Summoner x.y.z.dmg'
 create-dmg --overwrite ${DIST_DIR}/*.app --identity="${APPLE_DEVELOPER_ID_NAME}"
