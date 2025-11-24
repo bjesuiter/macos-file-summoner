aa70f597a127f045d4976a5b4f5f322ab210520f
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-11-24 20:09:43 +0100

    overhaul check-app-signature

--- scripts/04-check-app-signature.sh
+++ scripts/04-check-app-signature.sh
@@ -14,7 +14,3 @@ log "-- $(basename ${0}) --"
 
 log 'Check, whether the app is correctly signed and notarized'
 spctl -a -vvvv "${DIST_DIR}/${MACOS_APP_ARTIFACT}"
-
-log 'Some debug values from codesign tool'
-# Useful command for checking code signature in verbosity level 4 manually
-codesign -dv --verbose=4 "${DIST_DIR}/${MACOS_APP_ARTIFACT}"
